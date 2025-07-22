b9118d55768bba0fe6f7b0ac72ca4de66ce3945b
Srinivas Vedantam <srinivasvedantam@Srinivass-MacBook-Air.local>
2025-07-22 15:28:02 +0530

    feat: Prepare for decoupled deployment and Auth0

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -12,7 +12,7 @@ export async function apiRequest(
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const res = await fetch(url, {
+  const res = await fetch(`${API_BASE_URL}${url}`, {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
@@ -29,7 +29,7 @@ export const getQueryFn: <T>(options: {
 }) => QueryFunction<T> =
   ({ on401: unauthorizedBehavior }) =>
   async ({ queryKey }) => {
-    const res = await fetch(queryKey.join("/") as string, {
+    const res = await fetch(`${API_BASE_URL}$queryKey.join("/") as string`, {
       credentials: "include",
     });
 
